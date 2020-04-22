c25c472d2d7d4fba352ba6e49d5f7372512a7e24
Kevin Brinach <kevinbrinach@gmail.com>
2020-04-22 17:52:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import Clima from './Components/Clima';
+import Error from './Components/Error';
 
 function App() {
+
+  //state del formulario
+  const [ busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
+
+  const [ consultar, guardarConsultar ] = useState(false)
+  const [ resultado, guardarResultado ] = useState({})
+  const [ error, guardarError ] = useState(false)
+
+  //extraer ciudad y pais
+  const { ciudad, pais } = busqueda
+
+  useEffect( () => {
+    const consultarApi = async () => {
+      if (consultar) {
+        const appId = '4a75a664b930859da1a0e3416d80ee8f';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json()
+        guardarResultado(resultado);
+        guardarConsultar(false)
+
+        //Detecta si hubo resultados correctos
+        if(resultado.cod === '404') {
+          guardarError(true)
+        } else {
+          guardarError(false)
+        }
+      }
+    }
+    consultarApi()
+  }, [consultar])
+
+  let componente
+  if(error) {
+    componente = <Error mensaje="No Hay Resultados"/>
+  } else {
+    componente = <Clima 
+                  resultado={resultado}
+                />
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header
+        titulo='Clima React App'
+      ></Header>
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/Components/Formulario.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Error from './Error';
+
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
+
+    const [ error, guardarError ] = useState(false)
+
+    //extraer ciudad y pais
+    const { ciudad, pais } = busqueda
+
+    // funcion que coloca los elementos en el estate
+    const handleChange = (e) => {
+        // actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // enviar formulario
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+      //validar
+      if(ciudad.trim() === '' || pais.trim() === '') {
+          guardarError(true)
+          return;
+      }
+      guardarError(false)
+
+      //pasar al componente principal
+      guardarConsultar(true)
+    }
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <Error mensaje="Ambos Campos Son Obligatorios"/> : null}
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+            <div className="input-field col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                >
+                    <option value="">Seleccione un pais</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor="pais">Pais: </label>
+            </div>
+            <div className="input-field col s12">
+                <button
+                    type="submit"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4 col s12"
+                >Buscar Clima</button>
+            </div>
+        </form>    
+    );
+}
+ 
+export default Formulario;
